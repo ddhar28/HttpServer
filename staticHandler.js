601830230314ec501384d3b697e5c73f730f7571
Debi Dhar <ddhar2895@gmail.com>
2019-06-14 00:20:59 -0400

    Refactor code

--- staticHandler.js
+++ staticHandler.js
@@ -3,19 +3,12 @@ const path = require('path')
 const mime = require('mime-types')
 
 module.exports = (req, res) => {
-  const uri = req.startLine[1]
+  let uri = req.startLine[1]
 
-  if (uri === '/') {
-    res.header['Content-Type'] = mime.lookup('index.html')
-    fs.readFile('./public/index.html', (err, data) => {
-      if (err) console.log(err)
-      res.send(data)
-    })
-  } else if (uri.slice(1) !== 'favicon.ico') {
-    res.header['Content-Type'] = mime.lookup(uri.slice(1))
-    fs.readFile(path.join('./public', uri.slice(1)), (err, data) => {
-      if (err) console.log(err)
-      res.send(data)
-    })
-  }
+  if (uri === '/') uri += 'index.html'
+  res.header['Content-Type'] = mime.lookup(uri.slice(1))
+  fs.readFile(path.join('./public', uri.slice(1)), (err, data) => {
+    if (err) console.log(err)
+    res.send(data)
+  })
 }
